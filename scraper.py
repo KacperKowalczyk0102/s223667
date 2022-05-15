cb9890cfcad8ee174cc7344ba53a3052fe9d1381
KacperKowalczyk0102 <92937759+KacperKowalczyk0102@users.noreply.github.com>
2022-05-15 14:52:26 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,80 +1,62 @@
 import json
-import  requests
+import requests
 from bs4 import BeautifulSoup
 
-# s223667
+def extract_element(ancestor, selector, attribute=None, extract_list=False):
+    try:
+        if extract_list:
+            return ", ".join([item.text.strip() for item in ancestor.select(selector)])
+        if attribute:
+            return ancestor.select(selector).pop(0)[attribute].strip()
+        return ancestor.select(selector).pop(0).text.strip()
+    except IndexError: return None
 
-## Analiza struktury opinni w serwisie [Ceneo.pl]
+selectors = {
+    "author": ["span.user-post__author-name"],
+    "recommendation": ["span.user-post__author-recomendation > em"],
+    "stars": ["span.user-post__score-count"],
+    "content": ["div.user-post__text"],
+    "publish_date": ["span.user-post__published > time:nth-child(1)", "datetime"], 
+    "purchase_date": ["span.user-post__published > time:nth-child(2)", "datetime"],
+    "useful": ["span[id^=votes-yes]"],
+    "useless": ["span[id^=votes-no]"],
+    "pros": ["div.review-feature__title--positives ~ div.review-feature__item", None, True],
+    "cons": ["div.review-feature__title--negatives ~ div.review-feature__item", None, True]
+}
 
-url = "https://www.ceneo.pl/95365253;pla?se=YxWbm1iqQxdyrhZALD2q02WnsAqEsNg5&shop=146599625&gclid=EAIaIQobChMIz5SOvanU9gIVM0aRBR1U1Qi3EAQYASABEgI1O_D_BwE"
-response = requests.get(url)
-
-page_dom = BeautifulSoup(response.text, 'html.parser')
-
-reviews = page_dom.select("div.js_product-review")
-
+product_id = input("Podaj identyfokator produktu: ")
+url_pre = "https://www.ceneo.pl/"
+url_post = "/opinie-"
+page_no = 1
 all_reviews = []
 
+while(page_no):
+    url = url_pre+product_id+url_post+str(page_no)
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code == requests.codes.ok: 
+        page_dom = BeautifulSoup(response.text, 'html.parser')
+        reviews = page_dom.select("div.js_product-review")
+        for review in reviews: 
+            single_review = {
+                key: extract_element(review, *value)
+                    for key, value in selectors.items()
+            }
 
-for review in reviews: 
+            single_review["review_id"] = review["data-entry-id"]
+            single_review["recommendation"] = True if single_review["recommendation"] == "Polecam" else False if single_review["recommendation"] == "Nie polecam" else None
+            single_review["stars"] = float(single_review["stars"].split("/").pop(0).replace(",", "."))
+            single_review["content"] = single_review["content"].replace("\n", " ").replace("  ", " ").strip()
+            single_review["publish_date"] = single_review["publish_date"].split(" ").pop(0)
+            try:
+                single_review["purchase_date"] = single_review["purchase_date"].split(" ").pop(0)
+            except AttributeError:
+                single_review["purchase_date"] = None
+            single_review["useful"] = int(single_review["useful"])
+            single_review["useless"] = int(single_review["useless"])
 
-    review_id = review["data-entry-id"]
+            all_reviews.append(single_review)
+        page_no += 1
+    else: page_no = None
 
-    author = review.select("span.user-post__author-name").pop(0).text.strip()
-
-    try:
-        recommendation = review.select("span.user-post__author-recomendation > em").pop(0).text
-        recommendation = True if recommendation == "Polecam" else False
-    except: recommendation = None
-    stars = review.select("span.user-post__score-count").pop(0).text
-    stars = float(stars.split("/").pop(0).replace(",","."))
-
-    content = review.select("div.user-post__text").pop(0).get_text()
-
-    content = content.replace("\n"," ").replace("  "," ").strip()
-
-    publish_date = review.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"]
-    publish_date = publish_date.split("  ").pop(0)
-    #print(type(publish_date))
-    #print(publish_date)
-    try:
-        purchase_date =  review.select("span.user-post__published > time:nth-child(2)").pop(0)["datetime"]
-        #print(type(purchase_date))
-        #print(purchase_date)
-    except IndexError: purchase_date = None
-
-    useful = review.select("span[id^=votes-yes]").pop().text
-    useful = review.select("span[id^=votes-yes]").pop().text
-    useful = int(useful)
-
-    useless = review.select("span[id^=votes-no]").pop().text
-    useless = int(useful)
-
-    #print(type(useless))
-    #print(useless)
-
-    pros = review.select("div.review-feature__title--positives ~ div.review-feature__item")
-    pros = [item.text.strip() for item in pros]
-
-    cons = review.select("div.review-feature__title--negatives ~ div.review-feature__item")
-    cons = [item.text.strip() for item in cons]
-
-    #print(type(pros))
-    #print(pros)
-
-    single_review = {
-        "review_id": review_id,
-        "author": author,
-        "recomendation": recommendation,
-        "stars": stars,
-        "content": content,
-        "publish_date": publish_date,
-        "purchase_date": purchase_date,
-        "usefull": useful,
-        "useless": useless,
-        "pros": pros,
-        "cons": cons,
-    }
-    all_reviews.append(single_review)
-print(json.dumps(single_review, indent=4, ensure_ascii=False))
-#pretiffy robi nam wcięcia
+f = open("reviews/"+product_id+".json", "w", encoding="UTF-8")
+json.dump(all_reviews, f, indent=4, ensure_ascii=False)
